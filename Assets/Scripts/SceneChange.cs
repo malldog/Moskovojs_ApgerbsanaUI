d2c89132fe708db89488641f614657ba0308fcd1
Moskovojs <rihards.moskovojs05@gmail.com>
2023-05-02 10:46:55 +0300

    pievienoju paris opcijas game aina

---
+++ Assets/Scripts/SceneChange.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+public class SceneChange : MonoBehaviour
+{
+
+	public void UzSakumu()
+    {
+        SceneManager.LoadScene("Sakums", LoadSceneMode.Single);
+    }
+    public void Apturet()
+    {
+        Application.Quit();
+    }
+    public void UzMain()
+    {
+        SceneManager.LoadScene("Main", LoadSceneMode.Single);
+    }
+    public void UzInfo()
+    {
+        SceneManager.LoadScene("Info", LoadSceneMode.Single);
+    }
+    public void UzGame()
+    {
+        SceneManager.LoadScene("Game", LoadSceneMode.Single);
+    }
+}
+
